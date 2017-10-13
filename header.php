d7aadb155532e48002a10e8ec5277d30781cacf6
RemiW <32771735+RemiW@users.noreply.github.com>
2017-10-13 15:50:06 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php 
+    session_start();
+ ?>
+
+<!DOCTYPE html>
+<!--
+here
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="topnav">
+            <img src="logo.png" id="logo" alt="This is the logo of our association" title="Logo of the company" >
+            
+            <a href="product.html">Produits</a>
+            <a href="search.html"><img src="searchlogo.png" id="searchlogo">Recherche</a>
+            <a href="login.php" id="login">Authentification</a>
+            <a href="profile.php" id="profile"><?php echo $_SESSION['user']; ?> </a>
+        </div>
+    </body>
+</html>
