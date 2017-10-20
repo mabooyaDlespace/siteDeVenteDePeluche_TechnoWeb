4ef0bbbbbf7b90336fa15a84c0aacbbcd37a55c2
Dralok <dralok75@gmail.com>
2017-10-20 15:12:45 +0200

    Ajout de la page panier non fonctionnelle

---
+++ panier.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Rémi Willems, Loïc Schafran -->
+	<title>Panier</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script>
+            $(function () {
+                $("#myheader").load("header.html");
+                $("#myfooter").load("footer.html");
+            });
+        </script>
+</head>
+<body>
+
+<div id="myheader" ></div>
+
+
+<div id="myfooter"></div>
+
+</body>
+</html>
