4e37e2b763d66af408b2c288153a80ece5a0e210
RemiW <32771735+RemiW@users.noreply.github.com>
2017-10-20 15:31:21 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,46 @@
+<?php
+
+	include('database.php');
+	include('session.php');
+	include('user.php');
+
+	$deco = "";
+	if (!empty($sessionid)){
+		$name = infosUser($sessionid);
+		$deco = '<div id="lien"><a href="logout.php">Se déconnecter</a></div>';
+	}
+	else{
+		$name = 'visiteur';
+	}
+	$msg = "Bienvenue " .$name;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenue</title>
+	<meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script>
+            $(function () {
+                $("#myheader").load("header.php");
+                $("#myfooter").load("footer.html");
+            });
+        </script>
+</head>
+<body>
+	<div id="myheader" ></div>
+
+	<!-- <form action="" method="post">
+		<input class="pseudoEtMDP" type="submit" value="Se déconnecter" name="deconnexion">
+	</form> -->
+
+	<?php echo $deco ?>
+
+	<div id="msg"><?php echo $msg ?></div>
+
+	<div id="myfooter"></div>
+</body>
+</html>
