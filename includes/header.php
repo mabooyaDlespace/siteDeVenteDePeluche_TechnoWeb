7119dfac524f41c0787bf795aede1e5ac79f08b4
Hugo Stenger <hugo.stenger@isen.yncrea.fr>
2017-10-22 21:05:42 +0200

    Modif header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php
-$absoluteDirectoryName="http://localhost/TP/TP2/";
+$absoluteDirectoryName="http://localhost/sitedeventedepeluche_technoweb-firstnewbranche/";
 //$suitableWrapper='"'.$absoluteDirectoryName.'includes/checkSession"';
 require_once dirname(__FILE__).'/../database.php';
 require_once dirname(__FILE__).'/../session.php';
@@ -32,14 +32,15 @@ here
     <body>
         <header>
             <div class="topnav">
-                <a href="<?php echo $absoluteDirectoryName; ?>page_home/index.php" id="homelogo"><img src="<?php echo $absoluteDirectoryName; ?>images/logo.png" id="logo" title="Acceuil"></a>
+                <a href="<?php echo $absoluteDirectoryName; ?>page_home/index.php"><img src="<?php echo $absoluteDirectoryName; ?>images/logo.png" id="logo" title="Acceuil"></a>
                 <div class="topnavTexts">
                     <a href="<?php echo $absoluteDirectoryName; ?>pagePresentationDesProduits">Produits</a>
                     <a href="<?php echo $absoluteDirectoryName; ?>page_search/index.php"><img src="<?php echo $absoluteDirectoryName; ?>images/searchlogo.png" id="searchlogo">Recherche</a>
                     <a href="<?php echo $absoluteDirectoryName; ?>page_login/index.php" id="login">Authentification</a>
-                    <a class="topnav"> Session :<?php $temp1=returnUserConnection($sessionid);echo $temp1['name']; ?> </a>
                    <!--<?php $temp1=returnUserConnection();echo $temp1['name']; ?>  <?php echo $name = infosUser($sessionid); ?>
-                        --></div>
+                        -->
+                </div>
+                <a id="session"> Session :<?php $temp1=returnUserConnection($sessionid);echo $temp1['name']; ?> </a>
             </div>
         </header>
     </body>
