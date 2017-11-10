7f5faa4db065520beec2a9084475f5f77a162d83
mabooyaDlespace <simonnetjc@hotmail.fr>
2017-11-10 14:22:02 +0100

    retier nbproject

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php
-$absoluteDirectoryName="http://localhost/sitedeventedepeluche_technoweb-firstnewbranche/";
+$absoluteDirectoryName="http://localhost/TP/GitHubTP/";
 //$suitableWrapper='"'.$absoluteDirectoryName.'includes/checkSession"';
 require_once dirname(__FILE__).'/../database.php';
 require_once dirname(__FILE__).'/../session.php';
