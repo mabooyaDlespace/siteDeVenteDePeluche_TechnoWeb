b09224034c28bf33e57d546b6f08922d0a5c524b
Dralok <dralok75@gmail.com>
2017-11-10 17:04:27 +0100

    Commit avant pull

--- panier.php
+++ panier.php
@@ -1,7 +1,33 @@
+<?php
+
+	include('database.php');
+	include('store.php');
+	include('session.php');
+	include('user.php');
+	
+	$msgid = 0;
+	$msgquant = 0;
+	$msgErreur = "";
+	
+	if (!empty($_GET['quantite']) && !empty($_GET['id'])){
+		$quantite = $_GET['quantite'];
+		$id = $_GET['id'];
+		$achat = ajouterAuPanier($id, $quantite);
+		if($achat){
+			$msgid = $id;
+			$msgquant = $quantite;
+		}
+		else{
+			$msgErreur = "Erreur";
+		}
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- R�mi Willems, Lo�c Schafran -->
+	<!-- Rémi Willems, Loïc Schafran -->
 	<title>Panier</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
@@ -11,6 +37,9 @@
 
 <div id="myheader" > <?php require( "includes/header.php");?> </div>
 
+<div><?php echo $msgid; ?></div>
+<div><?php echo $msgquant; ?></div>
+<div><?php echo $msgErreur; ?></div>
 
 <div id="myfooter"> <?php include_once 'includes/footer.html'; ?></div>
 
