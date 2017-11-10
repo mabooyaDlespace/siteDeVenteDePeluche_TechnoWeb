48edaa13de18f4d6eef44c1baaa30cb4fe553bba
RemiW <remi.willems59@gmail.com>
2017-11-10 17:07:27 +0100

    product.php

--- product.php
+++ product.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
     include('database.php');
     include('store.php');
@@ -30,42 +29,25 @@
         }
     } */
 
-=======
-include('database.php');
-include('store.php');
-
-if (!empty($_GET['id'])) {
-    $id = $_GET['id'];
-    $infos = rechercherId($id);
-
-    $nameOfproduct = $infos['nom_produit'];
-    $desc = $infos['description'];
-    $price = $infos['prix'];
-    $image = $infos['image'];
-}
-
-if (!empty($_GET['add']) || !empty($_GET['quantite'])) {
-    $quantite = $_GET['quantite'];
-    $achat = ajouterAuPanier($id, $quantite);
-    if ($achat) {
-        header("Location: panier.php");
-    }
-}
->>>>>>> 6f7f5f6d1c082593e1f02b941a0c5b905a95c666
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<!-- Rémi Willems, Romain Sens et Dylane Sidelhadj -->
-	<title><?php echo $nameOfproduct ?></title>
+	<title>Page Produit</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
-        
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script>
+            $(function () {
+                $("#myheader").load("header.php");
+                $("#myfooter").load("footer.html");
+            });
+        </script>
 </head>
 <body>
 
-<<<<<<< HEAD
     <div id="myheader" ></div>
 
 <table>
@@ -82,28 +64,5 @@ if (!empty($_GET['add']) || !empty($_GET['quantite'])) {
 
 <div id="myfooter"></div>
 
-=======
-    <div id="myheader" >
-<?php
-require( "includes/header.php");
-?>
-        </div>
-    
-	<div><?php echo('<img src="images/' . $image . '.png" id="produit">') ?></div>
-	<H1 id="box1"><?php echo $nameOfproduct ?></H1>
-    <div id="box3">Description du produit : <br><br>
-		<?php echo $desc ?>
-    </div>
-    <div id="box2">
-		<form method="get">Quantité ? <input type="number" value="1" min="1" max="10000" name="quantite">
-        <br><br><br>
-        <div  id="price"><?php echo $price ?>€</div><br>
-		<div id="basket"></div><input type="submit" value="Ajouter au panier" id="addbasket" name="add"><div><img id="panier1" src="images/panier-ajout.png"></div></form>
-		
-    </div>
-
-<!--<div id="myfooter"></div>-->
-<div id="myfooter"> <?php include_once 'includes/footer.html'; ?></div>
->>>>>>> 6f7f5f6d1c082593e1f02b941a0c5b905a95c666
 </body>
 </html>
