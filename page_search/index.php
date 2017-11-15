52eef57ca84d8fb5e37e3a485c19c9426464f29c
MrOwl <hugo.stenger@isen.yncrea.fr>
2017-11-15 21:16:59 +0100

    rajout ?>

--- page_search/index.php
+++ page_search/index.php
@@ -1,12 +1,8 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 require 'model.php';
 $reponse_BDD=getResponse();
 $contenu_de_la_reponse_de_la_recherche= afficherPeluches($reponse_BDD);
 require 'view.php';
+
+?>
