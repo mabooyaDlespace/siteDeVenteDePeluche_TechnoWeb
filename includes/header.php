cdc37ebdc5ccc8feb21e4b8f7b3780c024b22771
nicolasroi <nicolas.roi@isen.yncrea.fr>
2017-12-01 14:43:04 +0100

    ajout de boutons vers profil et panier sur le header

--- includes/header.php
+++ includes/header.php
@@ -19,9 +19,13 @@ require_once dirname(__FILE__).'/checkSession.php';
             <div class="topnav">
                 <a href="<?php echo $absoluteDirectoryName; ?>page_home/index.php"><img src="<?php echo $absoluteDirectoryName; ?>images/logo.png" id="logo" title="Acceuil"></a>
                 <div class="topnavTexts">
-                    <!--<a href="<?php echo $absoluteDirectoryName; ?>pagePresentationDesProduits">Produits</a>-->
+
                     <a href="<?php echo $absoluteDirectoryName; ?>page_search/index.php"><img src="<?php echo $absoluteDirectoryName; ?>images/searchlogo.png" id="searchlogo">Recherche</a>
+                    <a href="<?php echo $absoluteDirectoryName; ?>page_search/index.php"><img src="<?php echo $absoluteDirectoryName; ?>images/panier-ajout.png" id="basketlogo">Panier</a>
+                    <a href="<?php echo $absoluteDirectoryName; ?>page_search/index.php"><img src="<?php echo $absoluteDirectoryName; ?>images/profil-logo.png" id="basketlogo">Profil</a>
+                    <a href="<?php echo $absoluteDirectoryName; ?>pagePresentationDesProduits">Produits</a>
                     <a href="<?php echo $absoluteDirectoryName; ?>page_login/index.php" id="login">Authentification</a>
+                    
                 </div>
                 <a id="session"> Session :<?php $temp1=returnUserConnection($sessionid);echo $temp1['name']; ?> </a>
             </div>
